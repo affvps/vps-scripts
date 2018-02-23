57d95af3c36d7f4b03cef19dc11632f14799140d
lmc920 <webdl.cc@gmail.com>
2018-02-23 23:44:25 +0800

    ZBench
    ZBench test scripts

--- zbench/ZBench-CN.sh
+++ zbench/ZBench-CN.sh
@@ -377,7 +377,7 @@ NetPiSM=$( sed -n "24p" /tmp/speed_cn.txt )
 NetUPCM=$( sed -n "25p" /tmp/speed_cn.txt )
 NetDWCM=$( sed -n "26p" /tmp/speed_cn.txt )
 NetPiCM=$( sed -n "27p" /tmp/speed_cn.txt )
-wget -N --no-check-certificate https://web-dl.cc/vps/script/Generate.py >> /dev/null 2>&1
+wget -N --no-check-certificate https://raw.githubusercontent.com/lmc920/vps-scripts/master/zbench/Generate.py >> /dev/null 2>&1
 python Generate.py && rm -rf Generate.py && cp /root/report.html /tmp/report/index.html
 TSM=$( cat /tmp/shm.txt_table )
 TST=$( cat /tmp/sht.txt_table )
