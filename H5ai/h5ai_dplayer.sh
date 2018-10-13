241f4910666c515fcc16bc8c5d960f7559632a56
lmc920 <webdl.cc@gmail.com>
2018-10-13 22:41:15 +0800

    fix
    fix

--- H5ai/h5ai_dplayer.sh
+++ H5ai/h5ai_dplayer.sh
@@ -13,10 +13,10 @@ wget https://raw.githubusercontent.com/lmc920/vps-scripts/master/H5ai/h5ai_dplay
 unzip h5ai_dplayer.zip
 mkdir /home/GoogleDrive
 cp -r /root/h5ai_dplayer/. /home/GoogleDrive
+nano /home/GoogleDrive/_h5ai/public/index.php #添加访问验证，第2行auth()的注释取消掉，第5行自己设置用户名及密码。
 
 # ADD SITE
 lnmp vhost add 
 
 # EDIT
-nano /usr/local/nginx/conf/vhost/yourdomain.conf #修改网站参数，index行添加“/_h5ai/public/index.php”
-nano /home/GoogleDrive/_h5ai/public/index.php #添加访问验证，第2行auth()的注释取消掉，第5行自己设置用户名及密码。
+# nano /usr/local/nginx/conf/vhost/yourdomain.conf #修改网站参数，index行添加“/_h5ai/public/index.php”
