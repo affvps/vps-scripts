76320f9eb0851a3920e5306c63121f5ab7d41d5a
Totoro <16508814+affvps@users.noreply.github.com>
2024-06-01 21:21:19 +0800

    Update rclone_debian.sh

--- rclone/rclone_debian.sh
+++ rclone/rclone_debian.sh
@@ -7,12 +7,9 @@ KernelBit="$(getconf LONG_BIT)"
 [[ "$KernelBit" == '64' ]] && KernelBitVer='amd64'
 [[ -z "$KernelBitVer" ]] && exit 1
 cd /tmp
-wget -O '/tmp/rclone.zip' "https://downloads.rclone.org/rclone-current-linux-$KernelBitVer.zip"
+wget --no-check-certificate -O '/tmp/rclone.zip' "https://downloads.rclone.org/rclone-current-linux-$KernelBitVer.zip"
 7z x /tmp/rclone.zip
 cd rclone-*
 cp -raf rclone /usr/bin/
 chown root:root /usr/bin/rclone
 chmod 755 /usr/bin/rclone
-mkdir -p /usr/local/share/man/man1
-cp -raf rclone.1 /usr/local/share/man/man1/
-mandb
